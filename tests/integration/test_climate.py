32aa4aff05fa7e976c25d4fca19776de9e366ea2
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2022-12-16 11:26:10 +1100

    Pass test

--- tests/integration/test_climate.py
+++ tests/integration/test_climate.py
@@ -1,3 +1,7 @@
+import os.path
+
+import pyam
+
 from climate_assessment.climate import climate_assessment
 
 
@@ -21,6 +25,7 @@ def test_climate_assessment_percentiles(
         "workflow-fair",
         "ex2_harmonized_infilled.csv",
     ))
+    df = df.filter(model=df.model[0])
 
     common_kwargs = dict(
         df=df,
@@ -47,4 +52,21 @@ def test_climate_assessment_percentiles(
         percentiles=percentiles_2,
     )
 
-    assert False, "Test different quantiles actually calculated"
+    for thresholds, peak_percentiles, percentiles, res in (
+        (temp_thresholds_1, peak_percentiles_1, percentiles_1, res_1),
+        (temp_thresholds_2, peak_percentiles_2, percentiles_2, res_2),
+    ):
+        for t in thresholds:
+            assert f"Exceedance Probability {t:.1f}C (FaIRv1.6.2)" in res.meta
+
+        for pp in peak_percentiles:
+            if pp == 50:
+                pp_s = "median"
+            else:
+                pp_s = f"p{pp:.0f}"
+
+            assert f"{pp_s} peak warming (FaIRv1.6.2)" in res.meta
+            assert f"{pp_s} year of peak warming (FaIRv1.6.2)" in res.meta
+
+        for p in percentiles:
+            assert f"AR6 climate diagnostics|Surface Temperature (GSAT)|FaIRv1.6.2|{p:.1f}th Percentile" in res.variable
