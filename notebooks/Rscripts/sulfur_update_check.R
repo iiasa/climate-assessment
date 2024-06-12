b3c5b47673e9b09f11df2819e02859165dd1c604
jkikstra <kikstra@iiasa.ac.at>
2024-06-12 09:57:24 +0200

    Use sulfur from CEDS2024 (instead of RCIMP/AR6)

--- notebooks/Rscripts/sulfur_update_check.R
+++ notebooks/Rscripts/sulfur_update_check.R
@@ -2,7 +2,8 @@
 # Do:
 # - [x] visualise RCMIP (used in AR6)
 # - [x] visualise CEDS 2024 update
-# - [ ] visualise difference between RCMIP and CEDS 
+# - [x] visualise difference between RCMIP and CEDS
+# - [ ] write out Sulfur data from CEDS for global emissions
 
 
 library(tidyverse)
@@ -14,13 +15,13 @@ here::i_am("README.rst")
 # Data -------------------------------------------------------------------------
 
 ### RCMIP ----------------------------------------------------------------------
-rcmip.emssions <- read_csv(here("src", 
+rcmip.emssions <- read_csv(here("src",
                                 "climate_assessment",
                                 "harmonization",
-                                "history_ar6.csv")) %>% 
+                                "history_ar6.csv")) %>%
   pivot_longer(cols = -c(Model,Scenario,Region,Variable,Unit),
                names_to = "Year",
-               values_to = "value") %>% 
+               values_to = "value") %>%
   mutate(Year = as.numeric(Year))
 
 ### CEDS 2024 ------------------------------------------------------------------
@@ -28,11 +29,11 @@ rcmip.emssions <- read_csv(here("src",
 ##### SO2 ----------------------------------------------------------------------
 ceds.emssions.sulfur <- read_csv(
   "C:/Users/kikstra/OneDrive - IIASA/_Other/Data/Emissions data/CEDS/CEDS_v_2024_04_01_aggregate/SO2_CEDS_emissions_by_country_v2024_04_01.csv"
-) %>% 
+) %>%
   pivot_longer(cols = -c(em,country,units),
                names_to = "Year",
-               values_to = "value") %>% 
-  mutate(Year = as.numeric(substr(Year, 2, 5))) %>% 
+               values_to = "value") %>%
+  mutate(Year = as.numeric(substr(Year, 2, 5))) %>%
   summarise(
     value = sum(value, na.rm = TRUE)*1e3/1e6,
     Unit = "Mt SO2/yr",
@@ -41,24 +42,33 @@ ceds.emssions.sulfur <- read_csv(
   )
 
 
+##### Save: SO2 emissions ------------------------------------------------------
+ceds.emssions.sulfur.wide <- ceds.emssions.sulfur %>% pivot_wider(names_from = Year, values_from = value)
+
+write_delim(
+  x = ceds.emssions.sulfur.wide,
+  file = here("notebooks", "Rscripts", "output-data", "sulfur_emissions_CEDS_global.csv"),
+  delim = ","
+)
+
 
 ### Future emissions (CMIP6) ---------------------------------------------------
-cmip6.emssions <- read_csv(here("src", 
+cmip6.emssions <- read_csv(here("src",
                                 "climate_assessment",
                                 "infilling",
-                                "cmip6-ssps-workflow-emissions.csv")) %>% 
+                                "cmip6-ssps-workflow-emissions.csv")) %>%
   pivot_longer(cols = -c(Model,Scenario,Region,Variable,Unit),
                names_to = "Year",
-               values_to = "value") %>% 
+               values_to = "value") %>%
   mutate(Year = as.numeric(Year))
 
 ### Future emissions (Infiller database AR6) -----------------------------------
-ar6.inf.emssions <- read_csv(here("tests", 
+ar6.inf.emssions <- read_csv(here("tests",
                                 "test-data",
-                                "ar6_vetted_infillerdatabase.csv")) %>% 
+                                "ar6_vetted_infillerdatabase.csv")) %>%
   pivot_longer(cols = -c(Model,Scenario,Region,Variable,Unit),
                names_to = "Year",
-               values_to = "value") %>% 
+               values_to = "value") %>%
   mutate(Year = as.numeric(Year))
 
 
@@ -77,7 +87,7 @@ sulfur.hist <- rcmip.emssions %>% filter(grepl(Variable,pattern="Sulfur",fixed=T
   mutate(Variable = "Sulfur (historical, AR6, RCMIP)") %>%
   bind_rows(ceds.emssions.sulfur)
 
-p.sulfur.hist <- ggplot(sulfur.hist %>% filter(Year >= 1970), 
+p.sulfur.hist <- ggplot(sulfur.hist %>% filter(Year >= 1970),
                         aes(x=Year, y=value, color=Variable)) +
   geom_line() +
   geom_hline(yintercept = 0, linetype = "dashed") +
@@ -98,21 +108,21 @@ sulfur.scens.cmip6 <- cmip6.emssions %>% filter(grepl(Variable,pattern="Sulfur",
 sulfur.scens.ar6 <- ar6.inf.emssions %>% filter(grepl(Variable,pattern="Sulfur",fixed=T)) %>%
   mutate(Variable = "Emissions|Sulfur (future, AR6 infiller database)")
 
-p.sulfur.scens <- ggplot(sulfur.hist %>% filter(Year >= 1990), 
+p.sulfur.scens <- ggplot(sulfur.hist %>% filter(Year >= 1990),
                         aes(x=Year, y=value, color=Variable)) +
   geom_line(linewidth=1.5) +
-  geom_line(data = sulfur.scens.ar6 %>% filter(Year>=2015,Year<=2050), 
+  geom_line(data = sulfur.scens.ar6 %>% filter(Year>=2015,Year<=2050),
                 aes(x=Year, y=value, group=interaction(Model,Scenario)),
                 colour = "grey",
                 linetype = "solid", alpha = 0.1) +
-  geom_line(data = sulfur.scens.ar6 %>% filter(Year>=2015,Year<=2050) %>% 
-              left_join(ar6.metadata) %>% 
-              filter(Category=="C1"), 
+  geom_line(data = sulfur.scens.ar6 %>% filter(Year>=2015,Year<=2050) %>%
+              left_join(ar6.metadata) %>%
+              filter(Category=="C1"),
             aes(x=Year, y=value, group=interaction(Model,Scenario)),
             colour = "dodgerblue",
             linetype = "solid", alpha = 0.1) +
-  geom_textline(data = sulfur.scens.cmip6 %>% filter(Year>=2015,Year<=2050), 
-                aes(x=Year, y=value, group=interaction(Model,Scenario), label = Scenario), 
+  geom_textline(data = sulfur.scens.cmip6 %>% filter(Year>=2015,Year<=2050),
+                aes(x=Year, y=value, group=interaction(Model,Scenario), label = Scenario),
                 linetype = "dashed",
                 colour = "black",
                 hjust=0.9) +
@@ -127,10 +137,10 @@ p.sulfur.scens <- ggplot(sulfur.hist %>% filter(Year >= 1990),
   theme_minimal()
 p.sulfur.scens
 
-ggsave(filename = here("notebooks", "Rscripts", "figures", "sulfur_emissions.pdf"), 
+ggsave(filename = here("notebooks", "Rscripts", "figures", "sulfur_emissions.pdf"),
        plot = p.sulfur.scens, device = cairo_pdf,
        width = 300, height = 200, dpi = 300, units = "mm")
-ggsave(filename = here("notebooks", "Rscripts", "figures", "sulfur_emissions.png"), 
+ggsave(filename = here("notebooks", "Rscripts", "figures", "sulfur_emissions.png"),
        plot = p.sulfur.scens,
        width = 300, height = 200, dpi = 300, units = "mm")
 
@@ -139,14 +149,14 @@ ggsave(filename = here("notebooks", "Rscripts", "figures", "sulfur_emissions.png
 # Visualisation (historical emissions difference) ------------------------------
 sulfur.hist.diff <- rcmip.emssions %>% filter(grepl(Variable,pattern="Sulfur",fixed=T)) %>%
   mutate(Variable = "Sulfur (historical, AR6, RCMIP)") %>%
-  bind_rows(ceds.emssions.sulfur) %>% 
-  select(Variable,Unit,Year,value) %>% 
-  pivot_wider(names_from = Variable, values_from = value) %>% 
+  bind_rows(ceds.emssions.sulfur) %>%
+  select(Variable,Unit,Year,value) %>%
+  pivot_wider(names_from = Variable, values_from = value) %>%
   mutate(diff = `SO2 (CEDS, Jan 2024 update)` - `Sulfur (historical, AR6, RCMIP)`) %>%
   mutate(diff.rel.to.rcmip = diff / `Sulfur (historical, AR6, RCMIP)`)
-  
 
-p.sulfur.hist.diff <- ggplot(sulfur.hist.diff %>% filter(Year >= 1970), 
+
+p.sulfur.hist.diff <- ggplot(sulfur.hist.diff %>% filter(Year >= 1970),
                         aes(x=Year, y=diff.rel.to.rcmip)) +
   geom_line() +
   geom_hline(yintercept = 0, linetype = "dashed") +
@@ -160,9 +170,99 @@ p.sulfur.hist.diff <- ggplot(sulfur.hist.diff %>% filter(Year >= 1970),
   theme_minimal()
 p.sulfur.hist.diff
 
-ggsave(filename = here("notebooks", "Rscripts", "figures", "sulfur_emissions_hist_diff.pdf"), 
+ggsave(filename = here("notebooks", "Rscripts", "figures", "sulfur_emissions_hist_diff.pdf"),
        plot = p.sulfur.hist.diff, device = cairo_pdf,
        width = 200, height = 100, dpi = 300, units = "mm")
-ggsave(filename = here("notebooks", "Rscripts", "figures", "sulfur_emissions_hist_diff.png"), 
+ggsave(filename = here("notebooks", "Rscripts", "figures", "sulfur_emissions_hist_diff.png"),
        plot = p.sulfur.hist.diff,
        width = 200, height = 100, dpi = 300, units = "mm")
+
+
+
+
+# Create new CMIP6 emissions dataset (all vetted scenarios) --------------------
+ar6.data.location.global <- "C:/Users/kikstra/OneDrive - IIASA/_Other/Data/Scenario data/Scenario Databases/AR6_Scenarios_Database_World_v1.1"
+ar6.data.file.global <- "AR6_Scenarios_Database_World_v1.1.csv"
+ar6.meta.file <- "AR6_Scenarios_Database_metadata_indicators_v1.1.xlsx"
+
+ar6.meta <- read_excel(file.path(ar6.data.location.global, ar6.meta.file),
+                       sheet = "meta"
+                       # sheet = "meta_Ch3vetted_withclimate"
+                       ) %>%
+  select(Model, Scenario,
+         Category) %>%
+  filter(Category!="failed-vetting")
+
+ar6.data.emissions <- vroom(
+  file.path(
+    ar6.data.location.global,
+    ar6.data.file.global
+  )
+) %>%
+  left_join(ar6.meta) %>%
+  drop_na(Category) %>% select(-Category) %>%
+  filter(str_detect(Variable, "^Emissions")) # in regex, the ^ symbol indicates the beginning of a string
+
+write_delim(
+  x = ar6.data.emissions,
+  file = here("notebooks", "Rscripts", "output-data", "ar6_emissions_vettedscenarios.csv"),
+  delim = ",",
+  na = ""
+)
+
+
+
+
+
+
+
+
+# Create new CMIP6 emissions dataset (all vetted scenarios) --------------------
+ar6.inf.emssions.newCEDSso2 <- read_csv(here("output",
+                                  "ar6_emissions_vettedscenarios_harmonized.csv")) %>%
+  pivot_longer(cols = -c(Model,Scenario,Region,Variable,Unit),
+               names_to = "Year",
+               values_to = "value") %>%
+  mutate(Year = as.numeric(Year))
+
+### new harmonized emissions (for infiller database) ---------------------------
+sulfur.scens.cmip6 <- cmip6.emssions %>% filter(grepl(Variable,pattern="Sulfur",fixed=T)) %>%
+  mutate(Variable = "Emissions|Sulfur (future, CMIP6)")
+sulfur.scens.ar6.newCEDSso2 <- ar6.inf.emssions.newCEDSso2 %>% filter(grepl(Variable,pattern="Sulfur",fixed=T)) %>%
+  mutate(Variable = "Emissions|Sulfur (future, AR6 infiller database)")
+
+p.sulfur.scens.newCEDSso2 <- ggplot(sulfur.hist %>% filter(Year >= 1990),
+                         aes(x=Year, y=value, color=Variable)) +
+  geom_line(linewidth=1.5) +
+  geom_line(data = sulfur.scens.ar6.newCEDSso2 %>% filter(Year>=2015,Year<=2050),
+            aes(x=Year, y=value, group=interaction(Model,Scenario)),
+            colour = "grey",
+            linetype = "solid", alpha = 0.1) +
+  geom_line(data = sulfur.scens.ar6.newCEDSso2 %>% filter(Year>=2015,Year<=2050) %>%
+              left_join(ar6.metadata) %>%
+              filter(Category=="C1"),
+            aes(x=Year, y=value, group=interaction(Model,Scenario)),
+            colour = "dodgerblue",
+            linetype = "solid", alpha = 0.1) +
+  geom_textline(data = sulfur.scens.cmip6 %>% filter(Year>=2015,Year<=2050),
+                aes(x=Year, y=value, group=interaction(Model,Scenario), label = Scenario),
+                linetype = "dashed",
+                colour = "black",
+                hjust=0.9) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  scale_color_manual(values = c("blue", "red")) +
+  labs(
+    title = "Historical sulfur emissions",
+    subtitle = "Grey: harmonized emissions AR6,\nLight blue: C1 scenarios",
+    x = "Year",
+    y = "Emissions (Mt SO2/yr)"
+  ) +
+  theme_minimal()
+p.sulfur.scens.newCEDSso2
+
+ggsave(filename = here("notebooks", "Rscripts", "figures", "sulfur_emissions_newCEDSso2.pdf"),
+       plot = p.sulfur.scens, device = cairo_pdf,
+       width = 300, height = 200, dpi = 300, units = "mm")
+ggsave(filename = here("notebooks", "Rscripts", "figures", "sulfur_emissions_newCEDSso2.png"),
+       plot = p.sulfur.scens,
+       width = 300, height = 200, dpi = 300, units = "mm")
