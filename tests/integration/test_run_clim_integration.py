c7d03e87f98c8f08e5b2a7d3521084c149478a49
Philip Hackstock <20710924+phackstock@users.noreply.github.com>
2025-07-22 16:42:18 +0200

    Update tests after click 8.2.x update

--- tests/integration/test_run_clim_integration.py
+++ tests/integration/test_run_clim_integration.py
@@ -90,7 +90,7 @@ def test_clim_historical_warming(
     assert (
         f"Adjusting median of {hist_eval_period[0:4]}-{hist_eval_period[5:9]} "
         f"warming (rel. to {hist_ref_period[0:4]}-{hist_ref_period[5:9]}) to {hist_warming}K"
-    ) in result.stdout
+    ) in result.output
 
     res = scmdata.ScmRun(
         os.path.join(out_dir, "ex2_harmonized_infilled_IAMC_climateassessment.xlsx"),
